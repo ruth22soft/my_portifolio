2779d3d507081b5f8b74b877d3a636bc0e4a15ad
Ruth <ruthtamiru10@gmail.com>
2025-05-03 06:59:08 +0300

    version1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I am Ruth , a Full Stack Developer and AI engineer based in Ethiopia.
           </p>
 
           <a href="#about">
